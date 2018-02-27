63ebd90a9aa9e5f92789d65bb50105e4a1a016b6
Robert Wilson <youmufan@gmail.com>
2018-02-26 20:01:09 -0500

    server side code

--- server/index.js
+++ server/index.js
@@ -1,2 +1,23 @@
-const express = requires('express');
-const axios = requires('axios');
+const express = requires('express'); //import express
+const axios = requires('axios');    //import axios
+
+const serverApp = express();    
+const port = process.env.PORT || 5000;
+
+serverApp.get('/forecast/:lat,:lon', function (req, res)
+{
+    const url = `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`
+    console.log(res);
+    axios.get(url)
+        .then( res => {
+            response.status(200).json(res)
+        })
+        .catch( error => {
+            response.status(500).json(error)
+        });
+});
+
+
+serverApp.listen(port, () => {
+    console.log(`Now listening on port ${port}.`)
+})
