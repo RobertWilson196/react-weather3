f7066ac86b037913a801a4a008e220609c731d74
Robert Wilson <youmufan@gmail.com>
2018-02-27 20:51:34 -0500

    added a timestamp method to convert into a readible time

--- client/src/HourlyWeather.js
+++ client/src/HourlyWeather.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
+import { convertTimeStamp } from "./utils";
 
-const HourlyWeather = (props) => 
-{
-    return(
-    <ul>
-        <li>Time: {props.time}</li>
-        <li>Temperature: {props.temperature}</li>
-    </ul>
-    );
-}
+const HourlyWeather = props => {
+  return (
+    <section>
+      <ul>
+        <li>Time: {convertTimeStamp(props.time)}</li>
+        <li>Temperature: {props.temperature}&deg;F</li>
+      </ul>
+    </section>
+  );
+};
 
 export default HourlyWeather;
---
+++ client/src/utils.js
@@ -0,0 +1,13 @@
+export const convertTimeStamp = (seconds) => 
+{
+    const d = new Date(seconds*1000); // s -> ms
+    //format : HH:MM mm/dd/YYYY 
+    const hours = d.getHours();
+    const minutes = d.getMinutes();
+
+    const year = d.getFullYear();
+    const month = d.getMonth() + 1;
+    const day = d.getDate();
+ 
+    return `${hours}:${minutes} ${month}/${day}/${year}` 
+}
